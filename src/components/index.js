e83427287574b789cf5e66097e65620dd0ef8cff
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-03-19 23:00:02 +0545

    making login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./Footer/Footer"
 import Container from "./Container/Container"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./Input.jsx"
+import Login from "./Login.jsx"
+import Button from "./Button.jsx"
 
-export { Header, Footer, Container, Logo, LogoutBtn }
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, Login }
