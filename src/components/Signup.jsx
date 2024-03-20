742d395da49ee16e5fa208e7bb1b65a0b60d6f37
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-03-20 08:38:10 +0545

    signup component created

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,59 +1,63 @@
 import React, { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { login as authLogin } from "../store/authSlice"
-import { Logo, Button, Input } from "./index"
-import { useDispatch } from "react-redux"
 import authService from "../appwrite/auth"
+import { Link, useNavigate } from "react-router-dom"
+import { login } from "../store/authSlice"
+import { Button, Logo, Input } from "./index"
+import { useDispatch } from "react-redux"
 import { useForm } from "react-hook-form"
 
-function Login() {
+function Signup() {
     const navigate = useNavigate()
+    const [error, setError] = useState("")
     const dispatch = useDispatch()
     const { register, handleSubmit } = useForm()
-    const [error, setError] = useState("")
 
-    const login = async (data) => {
+    const create = async (data) => {
         setError("")
         try {
-            session = await authService.login(data)
-            if (session) {
+            const userData = await authService.createAccount(data)
+            if (userData) {
                 const userData = await authService.getCurrentUser()
-                if (userData) dispatch(authLogin(userData))
+                if (userData) dispatch(login(userData))
                 navigate("/")
             }
         } catch (error) {
             setError(error.message)
         }
     }
-
     return (
-        <div className="flex items-center justify-center w-full">
+        <div className="flex items-center justify-center">
             <div
                 className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
             >
                 <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
+                    <span className="inline-block w-full min-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">
-                    Sign in to your account
+                    Sign up to create account
                 </h2>
                 <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
+                    Already have an account?&nbsp;
                     <Link
-                        to="/signup"
+                        to="/login"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
                     >
-                        Sign Up
+                        Sign In
                     </Link>
                 </p>
                 {error && (
                     <p className="text-red-600 mt-8 text-center">{error}</p>
                 )}
 
-                <form onSubmit={handleSubmit(login)} className="mt-8">
+                <form onSubmit={handleSubmit(create)}>
                     <div className="space-y-5">
+                        <Input
+                            label="Name: "
+                            placeholder="Enter your name"
+                            {...register("name", { required: true })}
+                        />
                         <Input
                             label="Email: "
                             placeholder="Enter your email"
@@ -61,30 +65,23 @@ function Login() {
                             {...register("email", {
                                 required: true,
                                 validate: {
-                                    matchPatern: (value) => (
+                                    matchPatern: (value) =>
                                         /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/gim.test(
                                             value
                                         ) ||
-                                            "Email address must be a valid address"
-                                    )
+                                        "Email address must be a valid address",
                                 },
                             })}
                         />
 
-                        <Input
-                            label="Password: "
-                            type="password"
-                            placeholder="Enter Your Password"
-                            {...register("password", {
-                                required: true,
-                            })}
-                        />
-                        <Button type="submit" className="">
-                            Sign In
-                        </Button>
+                        <Input label="Password: " type="password" placeholder="Enter Your Password" {...register("password", { required: true })} />
+
+                        <Button type="submit" className="w-full"> Create Account </Button>
                     </div>
                 </form>
             </div>
         </div>
     )
 }
+
+export default Signup
